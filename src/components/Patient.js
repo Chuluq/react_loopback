85770025a9234044f916aa93a626ba03b6eaaf71
Chuluq <choirulchuluq10@gmail.com>
2019-07-02 09:55:54 +0700

    fetch data

--- src/components/Patient.js
+++ src/components/Patient.js
@@ -1,66 +1,33 @@
 import React, {Component} from "react"
-import axios from 'axios'
 
 class Patient extends Component {
     constructor() {
         super()
         this.state = {
-            patients: []
+            allPatients: []
         }
     }
 
     componentDidMount() {
-        axios.get('http://localhost:3000/api/patients')
-        .then(response => {
-            // console.log(response.data)
-            this.setState({patients: response.data})
-        })
-        .catch(error => {
-            console.log(error)
-        })
+        fetch("http://localhost:3000/api/patients")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ allPatients : data })
+            }).catch(error => {
+                console.log(error)
+            })
     }
-    // componentWillMount() {
-    //     this.getPatients()
-    // }
-
-    // getPatients() {
-    //     axios.get('http://localhost:3000/api/patients')
-    //         .then(response => {
-    //             console.log(response)
-    //         })
-    // }
 
     render() {
-        const { patients } = this.state
+        const { allPatients } = this.state; 
+
         return(
             <div>
-                { patients.name }
+                {allPatients.map((patient, id) => (
+                    <p key={id}>{patient.name}</p>
+                ))}
             </div>
         )
-        // return(
-        //     <div>
-        //         <table>
-        //             <thead>
-        //                 <tr>
-        //                     <th>Name</th>
-        //                     <th>Gender</th>
-        //                     <th>Birth Date</th>
-        //                     <th>Address</th>
-        //                     <th>Phone</th>
-        //                 </tr>
-        //             </thead>
-        //             <tbody>
-        //                 <tr>
-        //                     <td>Ir. Roberto Firminho Sujadi</td>
-        //                     <td>Male</td>
-        //                     <td>1990-03-03</td>
-        //                     <td>Jl. Kusumanegara No.91, Umbulharjo, Kota Yogyakarta</td>
-        //                     <td>087741434432</td>
-        //                 </tr>
-        //             </tbody>
-        //         </table>
-        //     </div>
-        // )
     }
 }
 
